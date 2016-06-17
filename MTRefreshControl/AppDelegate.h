4d4d162990dbd51f0834b698a7dadd60c3c83f08
TianYaKe <liulong@hzins.com>
2016-06-17 18:02:47 +0800

    MT ..Create MTRefreshControl

---
+++ MTRefreshControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTRefreshControl
+//
+//  Created by long on 6/17/16.
+//  Copyright © 2016 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTRefreshControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTRefreshControl
+//
+//  Created by long on 6/17/16.
+//  Copyright © 2016 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
